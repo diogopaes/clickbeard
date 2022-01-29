88fb7cd7a8001961c44af7c8762060f80af26edc
Diogo Paes <diogopaesdev@gmail.com>
2022-01-29 15:44:50 -0300

    fear: handling errors

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { CreateUserController } from "./controllers/users/CreateUser";
+import { AuthController } from "./controllers/auth/AuthController";
+import { CreateController } from "./controllers/users/CreateController";
 
 const routes = Router();
 
-const createUserController = new CreateUserController();
+const createController = new CreateController();
+const authController = new AuthController();
 
-routes.post("/user/", createUserController.handle);
+routes.post("/user/", createController.handle);
+routes.post("/auth/", authController.handle);
 
 export { routes };
