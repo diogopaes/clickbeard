2856ac3436e25b8cb8ecfaef32457d356d62c411
Diogo Paes <diogopaesdev@gmail.com>
2022-01-31 01:35:19 -0300

    fix: cors erro add cors in to server

--- back/src/server.js
+++ back/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import "express-async-errors";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
