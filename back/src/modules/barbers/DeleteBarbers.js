1a4f32d50f514c264f6423a57bca27e00f5ba8f2
Diogo Paes <diogopaesdev@gmail.com>
2022-02-01 03:12:44 -0300

    fix: delete barber and schedules

--- back/src/modules/barbers/DeleteBarbers.js
+++ back/src/modules/barbers/DeleteBarbers.js
@@ -2,11 +2,17 @@ import { prisma } from "../../database/prismaClient";
 
 export class DeleteBarbers {
     async execute({id}) {
+        await prisma.scheduling.deleteMany({
+            where: {
+                id_barber: id
+            }
+        });
+
         const barber = await prisma.barber.delete({
             where: {
                 id
             }
-        })
+        });
 
         return barber;
     }
